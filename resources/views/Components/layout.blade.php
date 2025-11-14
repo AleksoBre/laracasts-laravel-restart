e11ec263083214f683afbdeb4a24842167dc9d34
AleksoBre <aleksstevancevic@gmail.com>
2025-11-14 16:00:55 +0100

    finished video 1.5

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -19,10 +19,10 @@
             </div>
             <div class="hidden md:block">
                 <div class="ml-10 flex items-baseline space-x-4">
-                <!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
-                <a href="/" aria-current="page" class="rounded-md bg-gray-950/50 px-3 py-2 text-sm font-medium text-white">Home</a>
-                <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">About</a>
-                <a href="/contact" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Contact</a>
+                <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+                <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
+                <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
+                {{-- ovo je nzs. class="rounded-md px-3 py-2 text-sm font-medium" --}}
                 </div>
             </div>
             </div>
@@ -37,7 +37,7 @@
     </header>
     <main>
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <!-- Your content -->
+        {{ $slot }}
         </div>
     </main>
     </div>
