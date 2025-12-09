4e7d940c84b1ec46a009aa5c84432bcb25f1f30c
AleksoBre <aleksstevancevic@gmail.com>
2025-12-09 22:05:43 +0100

    finished video 3.3

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -1,5 +1,9 @@
 <x-layout>
-    <x-slot:heading>{{$job['title']}}</x-slot:heading>
-    Pays: {{ $job['salary'] }}.
+    <x-slot:heading>{{ $job->title }}</x-slot:heading>
+    Pays: {{ $job->salary }}.
+    
+    <div>
+        <x-button class="mt-2" href="/jobs/{{ $job->id }}/edit">Edit</x-button>
+    </div>
 
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -8,23 +8,27 @@ Route::get('/', function () {
     return view('home');
 });
 
+// Index
 Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->latest()->simplePaginate(2);
+    $jobs = Job::with('employer')->latest()->simplePaginate(5);
 
     return view('jobs.index', [
         'jobs' => $jobs
     ]);
 });
 
+// Store
 Route::get('/jobs/create', function() {
     return view('jobs.create', ['employers' => Employer::all()]);
 });
 
+// Show
 Route::get('/jobs/{id}', function($id) { //bilo sta sa wildcard-om stavljam pri dnu routes fajla, jer ako stavim pri vrhu, onda bilo sta posle /jobs/ ce biti ukljuceno u ovu funkciju
     $job = Job::find($id);
     return view('jobs.show', ['job' => $job]);
 });
 
+// Insert
 Route::post('/jobs', function () {
     request()->validate([
         'job_title' => ['required', 'min:3'], 
@@ -39,6 +43,41 @@ Route::post('/jobs', function () {
 
     return redirect('jobs.index');
 });
+
+// Edit
+Route::get('/jobs/{id}/edit', function($id) {
+    return view('jobs.edit', ['job' => Job::findOrFail($id)]);
+});
+
+// Update
+Route::patch('/jobs/{id}', function($id) {
+    // authorize (On hold...)
+
+    //validacija
+    request()->validate([
+        'job_title' => ['required', 'min:3'], 
+        'job_salary' => ['required']
+    ]);
+
+    //menjamo podatke u bazi
+    Job::findOrFail($id)->update([
+        'title' => request('job_title'),
+        'salary' => request('job_salary')
+    ]);
+
+    //redirect
+    return redirect('jobs/' . $id);
+});
+
+// Destroy
+Route::delete('/jobs/{id}', function($id) {
+    // authorize (On hold...)
+
+    Job::findOrFail($id)->delete();
+
+    return redirect('/jobs');
+});
+
 
 
 
