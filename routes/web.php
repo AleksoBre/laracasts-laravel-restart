07e4933022c58a85be7b96ef7aa2cd94275aa4f4
AleksoBre <aleksstevancevic@gmail.com>
2025-12-10 14:36:39 +0100

    changed the routes to use Route::resource

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,9 @@ use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
-
-Route::controller(JobController::class)->group(function() {
-    Route::get('/jobs', 'index');
-    Route::get('/jobs/create', 'create');
-    Route::get('/jobs/{job}', 'show');
-    Route::post('/jobs', 'store');
-    Route::get('/jobs/{job}/edit', 'edit');
-    Route::patch('/jobs/{job}', 'update');
-    Route::delete('/jobs/{job}', 'destroy');
-});
-
 Route::view('/contact', 'contact');
+
+Route::resource('jobs', JobController::class); //'jobs' is the resource name and the URI
 
 
 
