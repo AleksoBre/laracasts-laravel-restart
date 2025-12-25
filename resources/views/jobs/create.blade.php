1537f1c9683b3e28d6e724962a72a06e1aecfdf3
AleksoBre <aleksstevancevic@gmail.com>
2025-12-25 13:16:33 +0100

    implemented new blade components

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -25,26 +25,37 @@
         </div>
 
         {{-- job title --}}
-        <div class="sm:col-span-4">
+        <x-form-field>
           <x-form-label for="job_title">Job Title</x-form-label>
           <div class="mt-2">
-            <x-form-input id="job_title" name="job_title" placeholder="eg. Carpenter" required></x-form-input>
+            <x-form-input id="job_title" name="job_title" placeholder="eg. Carpenter" required />
             <x-form-error name="job_title" />
           </div>
-        </div>
+        </x-form-field>
+
 
         {{-- job salary --}}
-        <div class="sm:col-span-4">
-          <label for="job_salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
+        <x-form-field>
+          <x-form-label for="job_salary">Salary</x-form-label>
           <div class="mt-2">
-            <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-              <input id="job_salary" type="text" name="job_salary" placeholder="eg. $80,000" required class="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"/>
-            </div>
-            @error('job_salary')
-              <p class="mt-1 text-red-500 text-xs font-semibold">{{ $message }}</p>
-            @enderror
+            <x-form-input id="job_salary" name="job_salary" placeholder="eg. $500,000 a year" required />
+            <x-form-error name="job_salary" />
           </div>
-        </div>
+        </x-form-field>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       </div>
     </div>
   </div>
