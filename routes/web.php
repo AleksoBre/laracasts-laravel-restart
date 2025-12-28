c8ac4bc30f9b1e1d6b5c1b6ea9aca4f5379a0879
AleksoBre <aleksstevancevic@gmail.com>
2025-12-28 11:16:25 +0100

    added register route and view

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,16 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
 Route::resource('jobs', JobController::class); //'jobs' is the resource name and the URI
+
+// Auth
+Route::get('/register', [RegisteredUserController::class, 'create']);
 
 
 
